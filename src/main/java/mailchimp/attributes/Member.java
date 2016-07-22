5767eb393fe99689bf784a5a21d6adbae0c463bc
José Vieira da Silva Neto <josevieira.dev@gmail.com>
2016-07-22 13:42:17 -0300

    adicionado hash code por e-mail em members

--- src/main/java/mailchimp/attributes/Member.java
+++ src/main/java/mailchimp/attributes/Member.java
@@ -590,4 +590,30 @@ public class Member {
 		this.totalItems = totalItems;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((emailAddress == null) ? 0 : emailAddress.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Member other = (Member) obj;
+		if (emailAddress == null) {
+			if (other.emailAddress != null)
+				return false;
+		} else if (!emailAddress.equals(other.emailAddress))
+			return false;
+		return true;
+	}
+
 }
